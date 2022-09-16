86bb0a4a0fbbdf03c1b623a625d29e53494f74cc
elynch78 <elynch78@charlottesvilleschools.org>
2022-09-16 10:44:40 -0400

    sg

--- raspberry-pi/LED_blink.py
+++ raspberry-pi/LED_blink.py
@@ -8,16 +8,17 @@ led2 = digitalio.DigitalInOut(board.GP18)
 led2.direction = digitalio.Direction.OUTPUT
 button = digitalio.DigitalInOut(board.GP16)
 button.direction = digitalio.Direction.INPUT
+button.pull = digitalio.Pull.UP
 
-if button.value == True:
-
-for x in reversed(range(11)):
-     led1.value = True
-     time.sleep(0.5)
-     print(x)
-     led1.value = False
-     time.sleep(0.5)
-while True:
-    print("liftoff!")
-    led2.value = True
-    time.sleep(0.5)
+while True: 
+     if button.value == False:
+          for x in reversed(range(11)):
+               led1.value = True
+               time.sleep(0.5)
+               print(x)
+               led1.value = False
+               time.sleep(0.5)
+          while True:
+               print("liftoff!")
+               led2.value = True
+               time.sleep(0.5)
