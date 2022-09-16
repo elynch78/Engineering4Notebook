7739ea1eb746d9cd5ff14a81e690df0e45b0a329
elynch78 <elynch78@charlottesvilleschools.org>
2022-09-16 10:44:47 -0400

    gesawyte

--- raspberry-pi/uploadtest.py
+++ raspberry-pi/uploadtest.py
@@ -1,1 +1,19 @@
-#deez
+import time
+import board
+import digitalio
+
+led1 = digitalio.DigitalInOut(board.GP13)
+led1.direction = digitalio.Direction.OUTPUT
+led2 = digitalio.DigitalInOut(board.GP18)
+led2.direction = digitalio.Direction.OUTPUT
+
+for x in reversed(range(11)):
+    led1.value = True
+    time.sleep(0.5)
+    print(x)
+    led1.value = False
+    time.sleep(0.5)
+while True:
+    print("liftoff!")
+    led2.value = True
+    time.sleep(0.5)
