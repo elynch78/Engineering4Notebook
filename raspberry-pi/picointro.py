176b73ba2b6a5517272cfa9494cc03f1a148aff5
elynch78 <elynch78@charlottesvilleschools.org>
2022-09-21 10:19:51 -0400

    vresgds

--- raspberry-pi/picointro.py
+++ raspberry-pi/picointro.py
@@ -1,26 +1,12 @@
 import time
 import board
 import digitalio
-import pwmio 
 
+led = digitalio.DigitalInOut(board.LED)
+led.direction = digitalio.Direction.OUTPUT
 
-led1 = digitalio.DigitalInOut(board.GP13)
-led1.direction = digitalio.Direction.OUTPUT
-led2 = digitalio.DigitalInOut(board.GP18)
-led2.direction = digitalio.Direction.OUTPUT
-button = digitalio.DigitalInOut(board.GP16)
-button.direction = digitalio.Direction.INPUT
-button.pull = digitalio.Pull.UP
-
-while True: 
-     if button.value == False:
-          for x in reversed(range(11)):
-               led1.value = True
-               time.sleep(0.5)
-               print(x)
-               led1.value = False
-               time.sleep(0.5)
-          while True:
-               print("liftoff!")
-               led2.value = True
-               time.sleep(0.5)
+while True:
+    led.value = True
+    time.sleep(0.5)
+    led.value = False
+    time.sleep(0.5)
