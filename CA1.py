4ff0559bb118e2bbfebc1338183620b84e637c56
elynch78 <elynch78@charlottesvilleschools.org>
2022-09-22 10:29:59 -0400

    gssrg

---
+++ CA1.py
@@ -0,0 +1,16 @@
+import time
+import board
+import adafruit_mpu6050
+import busio
+
+sda_pin = board.GP14
+scl_pin = board.GP15
+i2c = busio.I2C(scl_pin, sda_pin)
+mpu = adafruit_mpu6050.MPU6050(i2c)
+
+while True:
+    print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (mpu.acceleration))
+    print("Gyro X:%.2f, Y: %.2f, Z: %.2f rad/s" % (mpu.gyro))
+    print("Temperature: %.2f C" % mpu.temperature)
+    print("")
+    time.sleep(1)
