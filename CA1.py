d1dc3b6da9b8c5c73b17980f208f92eb0fdc7615
elynch78 <elynch78@charlottesvilleschools.org>
2022-09-22 10:45:26 -0400

    hrht

--- CA1.py
+++ CA1.py
@@ -1,16 +1,16 @@
-import time
+import time #imports stuff
 import board
 import adafruit_mpu6050
 import busio
 
-sda_pin = board.GP14
+sda_pin = board.GP14  #sets up i2c
 scl_pin = board.GP15
-i2c = busio.I2C(scl_pin, sda_pin)
+i2c = busio.I2C(scl_pin, sda_pin)  #sets up accelerometer
 mpu = adafruit_mpu6050.MPU6050(i2c)
 
 while True:
     print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (mpu.acceleration))
-    print("Gyro X:%.2f, Y: %.2f, Z: %.2f rad/s" % (mpu.gyro))
+    print("Gyro X:%.2f, Y: %.2f, Z: %.2f rad/s" % (mpu.gyro))  #things serial monitor needs to read
     print("Temperature: %.2f C" % mpu.temperature)
     print("")
     time.sleep(1)
