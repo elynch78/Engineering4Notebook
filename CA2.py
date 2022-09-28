dad1947b2d67b56310bf8b21377470ec1c4d385c
elynch78 <elynch78@charlottesvilleschools.org>
2022-09-28 10:39:40 -0400

    shsht

--- CA2.py
+++ CA2.py
@@ -1,19 +1,23 @@
-import time #imports stuff
-import board
+import board  #import shit
 import adafruit_mpu6050
-import busio
+import busio 
+import time
+import digitalio 
 
-sda_pin = board.GP14  #sets up i2c
+led_red = digitalio.DigitalInOut(board.GP18)   #led setup
+led_red.direction = digitalio.Direction.OUTPUT
+sda_pin = board.GP14   #setup pico
 scl_pin = board.GP15
-i2c = busio.I2C(scl_pin, sda_pin)  #sets up accelerometer
+i2c = busio.I2C(scl_pin, sda_pin)
 mpu = adafruit_mpu6050.MPU6050(i2c)
-led1 = digitalio.DigitalInOut(board.GP13)  #sets up led
-led1.direction = digitalio.Direction.OUTPUT
+
 
 while True:
-    if mpu.acceleration[0]:
-        led.value = True
-    if mpu.acceleration[1]:
-        led.value = True
+    print(mpu.acceleration)   #say the values
+    time.sleep(.5)
 
-        
+    if mpu.acceleration[0] < -9 or mpu.acceleration[0] > 9:
+        led_red.value = True  #at 90 degrees led is on
+
+    else:
+        led_red.value = False  #if not led is off
